6a55ee92bfc752a5be48fedd27fce2405fc7740b
tejaschache <tejas.chache3@gmail.com>
2024-08-12 18:38:40 +0530

    Hello

---
+++ src/main/java/com/ensat/entities/Product.java
@@ -0,0 +1,31 @@
+package com.ensat.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+
+/**
+ * Product entity.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    }
+
+}
